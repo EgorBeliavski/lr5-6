e1b0fbd992007636034e4b336d529b2c4282ef5b
EgorBeliavski <georgiebeliavski@gmail.com>
2024-12-20 21:57:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+string = open('text.txt','r',encoding="utf-8").read() # считывание текста из файла
+
+string = string.split() # разделение строки на список слов
+
+print("Количество слов:",len(string))
+
+string.close()
